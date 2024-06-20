9ea5044d4942c6ba35a56eabe1d23c04258fb985
Adeniran Shaddai <shaddaidan@gmail.com>
2024-06-20 10:20:59 +0100

    commited message

--- crypto_calc_past.py
+++ crypto_calc_past.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
 
 # load the historical price data
-df = pd.read_csv("crypto_data/btc-usd-max.csv")
+df = pd.read_csv('crypto_data/btc-usd-max.csv')
 
-# convery the snapped_at to datetime
-df['snapped_at'] = pd
+# convert the snapped_at to datetime
+df['snapped_at'] = pd.to_datetime(df['snapped_at'])
+
+# set the snapped_at as the index
